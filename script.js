da24d017594d022a329d6b136babb95534498ee1
Tanner Mascaro <tannermascaro@dhcp-10-5-85-55.ad.uvu.edu>
2026-03-23 17:33:28 -0600

    updated site

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const button = document.getElementById("bitch");
+const displayCount = document.getElementById('bitchCount');
+let bitchCount = 0;
 
+button.addEventListener('click', bitchIncrease);
+
+function bitchIncrease() {
+    bitchCount++;
+    displayCount.innerText = bitchCount;
+}
